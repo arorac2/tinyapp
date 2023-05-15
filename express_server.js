9740926414d2942b0e627d8e6116853863fa59e9
Chaahat <arorac2@mymacewan.ca>
2023-05-15 16:52:54 -0600

    updated everything to work

--- express_server.js
+++ express_server.js
@@ -4,6 +4,16 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters[randomIndex];
+    }
+    return randomString;
+  }
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -33,4 +43,27 @@ app.get("/urls", (req, res) => {
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
     res.render("urls_show", templateVars);
+  });
+
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  app.use(express.urlencoded({ extended: true }));
+
+  app.post("/urls", (req, res) => {
+    const shortURL = generateRandomString(); 
+    urlDatabase[shortURL] = req.body.longURL; 
+    const response = { shortURL }; 
+    res.status(200).json(response); 
+  });
+
+  app.get("/u/:id", (req, res) => {
+    const shortURL = req.params.id; 
+    const longURL = urlDatabase[shortURL]; 
+    if (longURL) {
+      res.redirect(longURL); // Redirect to the long URL
+    } else {
+      res.status(404).send("Short URL not found"); 
+    }
   });

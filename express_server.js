62ac52b833b7ac452a443ee1b63962819b5dd50e
Chaahat <arorac2@mymacewan.ca>
2023-05-16 15:42:50 -0600

    created a login button

--- express_server.js
+++ express_server.js
@@ -80,3 +80,8 @@ app.get("/urls", (req, res) => {
   
     res.redirect("/urls"); 
   });
+
+  app.post("/login", (req, res) => {
+    const { username } = req.body;
+    res.json({ message: `Logged in as ${username}` });
+  });
